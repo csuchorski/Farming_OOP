d4117d1f857c2a6db29799f4cca51da158e83c98
Cezary Suchorski <cezary.suchorski@gmail.com>
2024-12-30 15:52:56 +0100

    Spawning and starting threads for Dogs and Farmers

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,22 +1,40 @@
 package main;
 
+import model.Dog;
+import model.Farmer;
+import model.Field;
+import view.FieldView;
+
+import java.util.Vector;
+
 public class Main {
     public static void main(String[] args)
     {
         int fieldSize = 10;
         int farmerCount = 3;
         int turnsCount = 30;
-
-        Dog[] dogs = new Dog[farmerCount];
-        Farmer[] farmers = new Farmer[farmerCount];
         Field field = new Field(fieldSize);
+        Vector<Thread> threads = new Vector<>();
+
+        Vector<Dog> dogs = new Vector<>();
+        Vector<Farmer> farmers = new Vector<>();
+
 
         for (int i = 0; i < farmerCount; i++){
-            dogs[i] = new Dog(new int[]{0, 0});
-            farmers[i] = new Farmer(new int[]{0, 0},dogs[i]);
+            dogs.add(new Dog(new int[]{0,0}, field));
+            farmers.add(new Farmer(new int[]{0,0}, dogs.elementAt(i),field));
+
+            Thread dogThread = new Thread(dogs.get(i));
+            Thread farmerThread = new Thread(farmers.get(i));
+            threads.add(dogThread);
+            threads.add(farmerThread);
+            farmerThread.start();
+            dogThread.start();
         }
 
 
+        FieldView fieldView = new FieldView(field);
+        fieldView.setVisible(true);
 
     }
 }
